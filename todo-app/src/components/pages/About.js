2d72755c0247f4d00ad61b02ac54c73ef5a717b4
David Anusontarangkul <anusontarangkul.d@gmail.com>
2020-11-13 00:00:43 -0800

    add links in heading

--- todo-app/src/components/pages/About.js
+++ todo-app/src/components/pages/About.js
@@ -4,7 +4,7 @@ function About() {
     return (
         <React.Fragment>
             <h1>About</h1>
-            <p>This is the TodoList app.</p>
+            <p>This is a TodoList app.</p>
         </React.Fragment>
     )
 }
