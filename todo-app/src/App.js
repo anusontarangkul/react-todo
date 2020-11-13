c75dfc6f2f299023e17796a2f4d976c76c223c83
David Anusontarangkul <anusontarangkul.d@gmail.com>
2020-11-12 21:47:00 -0800

    add delete function

--- todo-app/src/App.js
+++ todo-app/src/App.js
@@ -25,13 +25,24 @@ class App extends Component {
   }
 
   markComplete = (id) => {
-    console.log(id)
+    this.setState({
+      todos: this.state.todos.map(todo => {
+        if (todo.id === id) {
+          todo.completed = !todo.completed
+        }
+        return todo;
+      })
+    })
+  }
+
+  delTodo = (id) => {
+    this.setState({ todos: [...this.state.todos.filter(todo => todo.id !== id)] })
   }
   render() {
 
     return (
       <div className="App">
-        <Todos todos={this.state.todos} markComplete={this.markComplete} />
+        <Todos todos={this.state.todos} markComplete={this.markComplete} delTodo={this.delTodo} />
       </div>
     )
   }
--- todo-app/src/components/TodoItems.js
+++ todo-app/src/components/TodoItems.js
@@ -20,6 +20,7 @@ class TodoItems extends Component {
                     <input type="checkbox" onChange={this.props.markComplete.bind(this, id)
                     } /> {' '}
                     {title}
+                    <button onClick={this.props.delTodo.bind(this, id)} style={btnStyle}>x</button>
                 </p>
             </div>
         )
@@ -27,4 +28,13 @@ class TodoItems extends Component {
 
 }
 
+const btnStyle = {
+    background: '#ff0000',
+    color: '#fff',
+    border: 'none',
+    padding: '5px 9px',
+    borderRadius: '50%',
+    cursor: 'pointer',
+    float: 'right'
+}
 export default TodoItems;
