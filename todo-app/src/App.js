617ca4cfc55d5bd5ad16fe5e1548b59254ac8ff9
David Anusontarangkul <anusontarangkul.d@gmail.com>
2020-11-12 23:33:12 -0800

    add functionality to addTodo

--- todo-app/src/App.js
+++ todo-app/src/App.js
@@ -40,13 +40,22 @@ class App extends Component {
   delTodo = (id) => {
     this.setState({ todos: [...this.state.todos.filter(todo => todo.id !== id)] })
   }
+
+  addTodo = (title) => {
+    const newTodo = {
+      id: 4,
+      title,
+      completed: false
+    }
+    this.setState({ todos: [...this.state.todos, newTodo] });
+  }
   render() {
 
     return (
       <div className="App">
         <div className="container">
           <Header />
-          <AddTodo />
+          <AddTodo addTodo={this.addTodo} />
           <Todos todos={this.state.todos} markComplete={this.markComplete} delTodo={this.delTodo} />
         </div>
       </div>
